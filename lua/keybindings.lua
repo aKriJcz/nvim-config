101139d17ea1f2fe1e14e581938ec77fecf5ed88
Jiří Kratochvíl <kratochvil.jiri.cz@gmail.com>
2025-01-06 14:49:36 +0100

    Added keybinding for file reference yank.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,6 +4,7 @@ wkey.setup {}
 
 -- Key bindings
 vim.api.nvim_set_keymap('n', '<Leader>H', ':call LongLineHLToggle()<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'yr', ':let @+ = join([expand("%"),  line(".")], ":")<cr>', { noremap = true }) -- FIXME: is dependent on timing between keys. Why?!
 
 -- Moving around in command mode
 --vim.api.nvim_set_keymap('c', '<C-j>', '<down>', { noremap = true })
