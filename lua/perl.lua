aa1c3d59830d22f822fc6afa066e1944a69f5196
Jiří Kratochvíl <kratochvil.jiri.cz@gmail.com>
2025-07-10 00:05:33 +0200

    WIP Perl HTML entities functions.

--- lua/perl.lua
+++ lua/perl.lua
@@ -1,8 +1,15 @@
 --
 -- Perl integration
 --
+if not vim.fn.has('perl') then
+    return
+end
 
 local wkey = require("which-key")
+
+local current_file = vim.api.nvim_get_runtime_file("lua/perl.lua", false)[1]
+local base_dir = vim.fn.fnamemodify(current_file, ':h')
+vim.cmd( "perlfile ".. base_dir .. "/../perl/Util.pm" )
 
 -- HTML entities coding
 -- https://vim.fandom.com/wiki/HTML_entities
@@ -30,7 +37,21 @@ endfunction
 ]]
 
 
+
+vim.cmd [[
+function! HTMLEncodeVisual()
+perl << EOF
+use Data::Dumper 'Dumper';
+use HTML::Entities;
+
+apply_selection(\&encode_entities);
+
+EOF
+endfunction
+]]
+
+
 wkey.add {
-  { "<Leader>ehe", ":call HTMLEncode()<CR>", mode = "nv", desc = "Encode characters to HTML characters" },
+  { "<Leader>ehe", ":call HTMLEncodeVisual()<CR>", mode = "nv", desc = "Encode characters to HTML characters" },
   { "<Leader>ehd", ":call HTMLDecode()<CR>", mode = "nv", desc = "Decode characters from HTML characters" },
 }
